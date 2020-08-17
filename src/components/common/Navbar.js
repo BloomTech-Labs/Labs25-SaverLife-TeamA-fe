c6822266ef84c7519b629e66c7ed42533ed4c84e
aurelio <aurelioarcabascio@gmail.com>
2020-08-17 13:25:47 -0400

    FIX: Remodeled Navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined } from '@ant-design/icons';
+
 import '../../styles/Navbar.css';
 
 const Navbar = props => {
+  const [current, setCurrent] = useState('mail');
+
+  const handleClick = e => {
+    setCurrent(e.key);
+  };
+
   return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/">Past Spending</Link>
-      <Link to="/">Budget Comparison</Link>
-      <Link to="/">Projected Savings</Link>
-    </nav>
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Menu.Item key="home" icon={<MailOutlined />}>
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="past-spending" icon={<AppstoreOutlined />}>
+        <Link to="/">Past Spending</Link>
+      </Menu.Item>
+      <Menu.Item key="budget-comparison" icon={<AppstoreOutlined />}>
+        <Link to="/">Budget Comparison</Link>
+      </Menu.Item>
+      <Menu.Item key="projected-savings" icon={<AppstoreOutlined />}>
+        <Link to="/">Projected Savings</Link>
+      </Menu.Item>
+    </Menu>
   );
 };
 
