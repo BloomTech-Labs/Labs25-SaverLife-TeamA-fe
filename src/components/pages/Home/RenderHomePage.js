40e0b0400c1c9610b20d5be69158b2bae2be0816
aurelio <aurelioarcabascio@gmail.com>
2020-08-17 13:13:54 -0400

    FEAT: Added Pie Chart with dummy data

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,7 +1,20 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import { Button, Progress } from 'antd';
 import { Navbar } from '../../common/index';
+import Plot from 'react-plotly.js';
+
+var data = [
+  {
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie',
+  },
+];
+
+var layout = {
+  height: 400,
+  width: 600,
+};
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
@@ -12,12 +25,14 @@ function RenderHomePage(props) {
       <div>
         <h1>Progress towards Goal</h1>
         <Progress
-          strokeColor={{
-            '0%': '#ecb7db',
-            '100%': '#c01089',
-          }}
+          strokeColor={{ '0%': '#ecb7db', '100%': '#c01089' }}
           percent={99}
         />
+      </div>
+      <h1>Deadline: 30 Days</h1>
+      <div style={{ border: '1px solid red' }}>
+        <Plot data={data} layout={layout} />
+        <h1>Current Spending</h1>
       </div>
       <Button type="primary" onClick={() => authService.logout()}>
         Logout
