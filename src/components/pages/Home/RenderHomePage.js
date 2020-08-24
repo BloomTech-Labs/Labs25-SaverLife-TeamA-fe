8908ef3ba3d5321e1e3490a8d460baf586030552
aurelio <aurelioarcabascio@gmail.com>
2020-08-24 18:33:02 -0400

    FEAT: Made API request function but CORS said nope

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,9 +1,33 @@
 import React from 'react';
-// import Plot from 'react-plotly.js';
+import Plot from 'react-plotly.js';
 import { Progress } from 'antd';
+import axios from 'axios';
 import { Navbar } from '../../common/index';
 
 import '../../../styles/Navbar.css';
+
+// CORS issue, can't continue working on this till fixed.
+const getGraphData = () => {
+  const userData = {
+    user_ID: '1635ob1dkQIz1QMjLmBpt0E36VyM96ImeyrgZ',
+    graph_type: 'pie',
+    time_period: 'week',
+  };
+
+  let data;
+
+  axios
+    .post(
+      'http://saverlife-a.eba-atdfhqrp.us-east-1.elasticbeanstalk.com/spending',
+      userData
+    )
+    .then(res => {
+      data = JSON.parse(res.data);
+      console.log(data);
+    });
+
+  return <Plot data={data.data} layout={data.layout} />;
+};
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
@@ -26,9 +50,7 @@ function RenderHomePage(props) {
         <h2>Deadline: 30 Days</h2>
 
         <div className="mainContent">
-          <div className="chartContainer">
-            <h1>Container Holding Chart</h1>
-          </div>
+          <div className="chartContainer">{/* {getGraphData()} */}</div>
           <h1>Current Spending</h1>
         </div>
       </div>
