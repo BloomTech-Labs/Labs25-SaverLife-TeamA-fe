1d6cb1a7bcfb91a49491bf2c15c02468910a36a7
Manuel De La Mora <Manuel@defaults-MBP.attlocal.net>
2020-09-01 15:33:57 -0700

    FIX: Still working on stylings and fixed complications when it came to the budget visuals for the home page.

---
+++ src/components/EnhancedProgressBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const EnhancedProgressBar = props => {
+  console.log('ProgressBar Props', props);
+  const categoryName = props.entry[0];
+  const categoryGoal = props.entry[1];
+  const categoryCurrent = props.spendingCurrent[categoryName];
+  const percentFilled = (categoryCurrent / categoryGoal) * 100;
+  const spendingStatus = () => {
+    if (percentFilled < 60) {
+      return 'success';
+    } else if (percentFilled < 90) {
+      return 'warning';
+    } else {
+      return 'danger';
+    }
+  };
+  const goalRendering = () => (props.isEditing ? '' : `$${categoryGoal}`);
+  // if (!props.isEditing){
+  //     return `$${categoryGoal}`
+  // }
+  // else {
+  //     return <input name={categoryName} value={categoryGoal}/>
+  // }
+
+  // console.log(categoryCurrent, category)
+  return (
+    <div style={styles}>
+      <div className="progressHeader" style={headerStyles}>
+        <h6>{categoryName}</h6>
+        <p>{`Spent: $${(categoryCurrent + 0).toFixed(2)} | Remaining: $${(
+          categoryGoal - categoryCurrent
+        ).toFixed(2)} | Goal: ${goalRendering()}`}</p>
+      </div>
+      <ProgressBar striped variant={spendingStatus()} now={percentFilled} />
+    </div>
+  );
+};
+const headerStyles = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexDirection: 'row',
+  marginBottom: -10,
+};
+
+const styles = {
+  marginBottom: '15px',
+  width: '70%',
+};
+
+export default EnhancedProgressBar;
